dc8abafd8459337d5d76061ad7fdce7fdfe4e3a9
wangyang <missgoon@163.com>
2016-03-10 16:04:48 +0800

    dev

---
+++ config/initializers/assets.rb
@@ -0,0 +1,18 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+# Rails.application.config.assets.precompile += %w( jquery.min.js )
+# Rails.application.config.assets.precompile += %w( easing.js )
+# Rails.application.config.assets.precompile += %w( jquery-1.11.0.min.js )
+# Rails.application.config.assets.precompile += %w( jquerymixitup.min.js )
+# Rails.application.config.assets.precompile += %w( jquery.swipebox.min.js )
+# Rails.application.config.assets.precompile += %w( move-top.js )
+# Rails.application.config.assets.precompile += %w(responsiveslides.min.js)
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
